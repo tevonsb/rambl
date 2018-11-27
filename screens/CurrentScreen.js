8dcec15db68b3223041842c177e6fda2af28f399
Tevon Strand-Brown <tevon.strandbrown@gmail.com>
2018-11-26 21:32:47 -0800

    added spinners

--- screens/CurrentScreen.js
+++ screens/CurrentScreen.js
@@ -7,31 +7,69 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Picker,
 } from 'react-native';
 import { MapView } from 'expo';
 
 export default class CurrentScreen extends React.Component {
   static navigationOptions = {
-    title: 'Rambl',a
+    title: 'Rambl',
   };
   constructor(props){
     super(props);
     this.state = {
-      currentView = "choose", //Options are Choose, suggest, rambl
-    }
+      currentView: "choose",
+    };
   }
+  getPickerMinutes(){
+  }
+
+  getPickerHours(){
+  }
+  getComponentForState(){
+    if(this.state.currentView === "choose"){
+      return (
+        <View
+          style={{flex: 1}}
+          >
+          <Text>How long do you have to spend?</Text>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <Text>Hours</Text>
+            <Picker
+              style={{ height: 50, width: 100 }}
+              >
+              <Picker.Item label="One" value="1" />
+              <Picker.Item label="Two" value="2" />
+              <Picker.Item label="Three" value="3" />
+            </Picker>
+            <Text>Minutes</Text>
+            <Picker
+              style={{ height: 50, width: 100 }}
+              >
+              <Picker.Item label="One" value="1" />
+              <Picker.Item label="Two" value="2" />
+              <Picker.Item label="Three" value="3" />
+            </Picker>
+          </View>
+        </View>
+      )
+    }
+    return null;
+  }
+
   render() {
-    return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    );
+    return this.getComponentForState();
+    // return (
+    //   <MapView
+    //     style={{ flex: 1 }}
+    //     initialRegion={{
+    //       latitude: 37.78825,
+    //       longitude: -122.4324,
+    //       latitudeDelta: 0.0922,
+    //       longitudeDelta: 0.0421,
+    //     }}
+    //   />
+    // );
   }
 }
 
