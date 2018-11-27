839a2f2e216839e153e6c14b107001f1b425a77e
Tevon Strand-Brown <tevon.strandbrown@gmail.com>
2018-11-26 22:00:17 -0800

    Added minute and hour selectors

--- screens/CurrentScreen.js
+++ screens/CurrentScreen.js
@@ -19,12 +19,28 @@ export default class CurrentScreen extends React.Component {
     super(props);
     this.state = {
       currentView: "choose",
+      hour: 1,
+      minute: 15,
     };
   }
   getPickerMinutes(){
+    const minutes = [0, 15, 30, 45];
+    return minutes.map((number) =>
+          <Picker.Item key={number.toString()} label={number.toString()} value={number}/>);
   }
 
   getPickerHours(){
+    const hours = [0, 1, 2, 3, 4, 5];
+    return hours.map((number) =>
+          <Picker.Item key={number.toString()} label={number.toString()} value={number}/>);
+  }
+
+  getRambls(){
+    return this.props.screenProps.rambls.map((rambl) => {
+      if(rambl.duration > this.state.hour * 60 + this.state.minute){
+        return (<li>rambl.title</li>)
+      }
+    });
   }
   getComponentForState(){
     if(this.state.currentView === "choose"){
@@ -36,21 +52,27 @@ export default class CurrentScreen extends React.Component {
           <View style={{flex: 1, flexDirection: 'row'}}>
             <Text>Hours</Text>
             <Picker
+              selectedValue={this.state.hour}
               style={{ height: 50, width: 100 }}
+              onValueChange={(itemValue, itemIndex) => this.setState({hour: itemValue})}
               >
-              <Picker.Item label="One" value="1" />
-              <Picker.Item label="Two" value="2" />
-              <Picker.Item label="Three" value="3" />
+              {this.getPickerHours()}
             </Picker>
             <Text>Minutes</Text>
             <Picker
+              selectedValue={this.state.minute}
+              onValueChange={(itemValue, itemIndex) => this.setState({minute: itemValue})}
               style={{ height: 50, width: 100 }}
               >
-              <Picker.Item label="One" value="1" />
-              <Picker.Item label="Two" value="2" />
-              <Picker.Item label="Three" value="3" />
+              {this.getPickerMinutes()}
             </Picker>
           </View>
+          <Text>Your Rambl is currently for {this.state.hour} and {this.state.minute} minutes.</Text>
+        </View>
+        <View>
+          <ul>
+          {this.getRambls()}
+          </ul>
         </View>
       )
     }
