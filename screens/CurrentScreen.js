3cdd198697de235752b5dd003affa58204199fd7
Tevon Strand-Brown <tevon.strandbrown@gmail.com>
2018-11-26 22:27:53 -0800

    Added dynamic rambl list

--- screens/CurrentScreen.js
+++ screens/CurrentScreen.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   View,
   Picker,
+  FlatList,
 } from 'react-native';
 import { MapView } from 'expo';
 
@@ -22,83 +23,85 @@ export default class CurrentScreen extends React.Component {
       hour: 1,
       minute: 15,
     };
+    this.checkDuration = this.checkDuration.bind(this);
   }
   getPickerMinutes(){
     const minutes = [0, 15, 30, 45];
     return minutes.map((number) =>
-          <Picker.Item key={number.toString()} label={number.toString()} value={number}/>);
-  }
-
-  getPickerHours(){
-    const hours = [0, 1, 2, 3, 4, 5];
-    return hours.map((number) =>
-          <Picker.Item key={number.toString()} label={number.toString()} value={number}/>);
-  }
-
-  getRambls(){
-    return this.props.screenProps.rambls.map((rambl) => {
-      if(rambl.duration > this.state.hour * 60 + this.state.minute){
-        return (<li>rambl.title</li>)
-      }
-    });
-  }
-  getComponentForState(){
-    if(this.state.currentView === "choose"){
-      return (
-        <View
-          style={{flex: 1}}
-          >
-          <Text>How long do you have to spend?</Text>
-          <View style={{flex: 1, flexDirection: 'row'}}>
-            <Text>Hours</Text>
-            <Picker
-              selectedValue={this.state.hour}
-              style={{ height: 50, width: 100 }}
-              onValueChange={(itemValue, itemIndex) => this.setState({hour: itemValue})}
-              >
-              {this.getPickerHours()}
-            </Picker>
-            <Text>Minutes</Text>
-            <Picker
-              selectedValue={this.state.minute}
-              onValueChange={(itemValue, itemIndex) => this.setState({minute: itemValue})}
-              style={{ height: 50, width: 100 }}
-              >
-              {this.getPickerMinutes()}
-            </Picker>
-          </View>
-          <Text>Your Rambl is currently for {this.state.hour} and {this.state.minute} minutes.</Text>
-        </View>
-        <View>
-          <ul>
-          {this.getRambls()}
-          </ul>
-        </View>
-      )
+    <Picker.Item key={number.toString()} label={number.toString()} value={number}/>);
     }
-    return null;
-  }
 
-  render() {
-    return this.getComponentForState();
-    // return (
-    //   <MapView
-    //     style={{ flex: 1 }}
-    //     initialRegion={{
-    //       latitude: 37.78825,
-    //       longitude: -122.4324,
-    //       latitudeDelta: 0.0922,
-    //       longitudeDelta: 0.0421,
-    //     }}
-    //   />
-    // );
-  }
-}
+    getPickerHours(){
+      const hours = [0, 1, 2, 3, 4, 5];
+      return hours.map((number) =>
+      <Picker.Item key={number.toString()} label={number.toString()} value={number}/>);
+      }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
+      checkDuration(rambl){
+        return rambl.duration < this.state.hour * 60 + this.state.minute;
+      }
+      getRambls(){
+        return this.props.screenProps.rambls.filter(this.checkDuration);
+      }
+      getComponentForState(){
+        if(this.state.currentView === "choose"){
+          return (
+            <View
+              style={{flex: 1}}
+              >
+              <Text>How long do you have to spend?</Text>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                <Text>Hours</Text>
+                <Picker
+                  selectedValue={this.state.hour}
+                  style={{ height: 50, width: 100 }}
+                  onValueChange={(itemValue, itemIndex) => this.setState({hour: itemValue})}
+                  >
+                  {this.getPickerHours()}
+                </Picker>
+                <Text>Minutes</Text>
+                <Picker
+                  selectedValue={this.state.minute}
+                  onValueChange={(itemValue, itemIndex) => this.setState({minute: itemValue})}
+                  style={{ height: 50, width: 100 }}
+                  >
+                  {this.getPickerMinutes()}
+                </Picker>
+              </View>
+              <Text>Your Rambl is currently for {this.state.hour} and {this.state.minute} minutes.</Text>
+              <View style={{flex: 1, padding: 22}}>
+                <FlatList
+                  data={this.getRambls()}
+                  renderItem={({item}) => <Text>{item.title}</Text>}
+                />
+
+              </View>
+            </View>
+          )
+        }
+        return null;
+      }
+
+      render() {
+        return this.getComponentForState();
+        // return (
+        //   <MapView
+        //     style={{ flex: 1 }}
+        //     initialRegion={{
+        //       latitude: 37.78825,
+        //       longitude: -122.4324,
+        //       latitudeDelta: 0.0922,
+        //       longitudeDelta: 0.0421,
+        //     }}
+        //   />
+        // );
+      }
+    }
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        paddingTop: 15,
+        backgroundColor: '#fff',
+      },
+    });
