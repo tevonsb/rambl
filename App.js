8e56e9c54bc359bf843b0c408b1918c9002e36b2
Clare Chen <clarechen101@gmail.com>
2018-11-27 19:23:39 -0800

    added username and location to state

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { AppLoading, Asset, Font, Icon } from "expo";
+import AppNavigator from "./navigation/AppNavigator";
 
-var rambls = require('./data/rambls');
-var users = require('./data/users');
-var friends_rambls = require('./data/friends_rambls');
-var past_rambls = require('./data/past_rambls');
+var rambls = require("./data/rambls");
+var users = require("./data/users");
+var friends_rambls = require("./data/friends_rambls");
+var past_rambls = require("./data/past_rambls");
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-        isLoadingComplete: false,
-        testText: "",
-        //Add additional state variables here
-      };
-      this.setGlobalState = this.setGlobalState.bind(this);
+      isLoadingComplete: false,
+      testText: "",
+      //Add additional state variables here
+      username: "Amanda",
+      location: "San Francisco"
+    };
+    this.setGlobalState = this.setGlobalState.bind(this);
   }
 
-  setGlobalState(newState){
+  setGlobalState(newState) {
     this.setState(newState);
   }
 
@@ -39,12 +41,12 @@ export default class App extends React.Component {
         rambls: rambls,
         users: users,
         friends_rambls: friends_rambls,
-        past_rambls: past_rambls,
+        past_rambls: past_rambls
       };
       return (
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator screenProps = {screenProps}/>
+          {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+          <AppNavigator screenProps={screenProps} />
         </View>
       );
     }
@@ -53,16 +55,16 @@ export default class App extends React.Component {
   _loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
+        require("./assets/images/robot-dev.png"),
+        require("./assets/images/robot-prod.png")
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
+        "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf")
+      })
     ]);
   };
 
@@ -80,6 +82,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
