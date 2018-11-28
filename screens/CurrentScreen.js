84019441de3534c80ecb193b529bda44ec935361
Tevon Strand-Brown <tevon.strandbrown@gmail.com>
2018-11-27 21:41:17 -0800

    ADDED TO COMPONENT

--- screens/CurrentScreen.js
+++ screens/CurrentScreen.js
@@ -54,7 +54,7 @@ export default class CurrentScreen extends React.Component {
       getComponentForState(){
         if(this.state.currentView === "selected"){
           return (
-            <RamblDetailComponent rambl={this.state.currentRambl}/>
+            <RamblDetailScreenComponent rambl={this.state.currentRambl}/>
           )
         }
         if(this.state.currentView === "choose"){
