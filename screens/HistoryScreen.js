b229503efce009a4e3e203aa18607ec9eafec577
azwarens <azwarenstein@gmail.com>
2018-11-28 13:11:53 -0800

    fixed bugs`

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import {
   Image,
@@ -18,53 +18,63 @@ import { MonoText } from '../components/StyledText';
 import RamblDetailComponent from './RamblDetailScreen.js'
 import LoadingScreenComponent from './LoadingScreen.js'
 
-
 export default class HistoryScreen extends React.Component {
   static navigationOptions = {
-    title: 'Past Rambls',
+    title: "Past Rambls"
   };
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-        selectedIndex: 0,
-        testText: "",
-        values: ['My Rambls', 'Friends\' Rambls'],
-        value: 'My Rambls',
-        currentView: 'unselected',
-      };
-      this._onChange = this._onChange.bind(this);
-      this._onValueChange = this._onValueChange.bind(this);
-      this.getMyRambls = this.getMyRambls.bind(this);
-      this.getFriendsRambls = this.getFriendsRambls.bind(this);
-      this.handleRamblPress = this.handleRamblPress.bind(this);
+      selectedIndex: 0,
+      testText: "",
+      values: ["My Rambls", "Friends' Rambls"],
+      value: "My Rambls",
+      currentView: "unselected"
+    };
+    this._onChange = this._onChange.bind(this);
+    this._onValueChange = this._onValueChange.bind(this);
+    this.getMyRambls = this.getMyRambls.bind(this);
+    this.getMyUsername = this.getMyUsername.bind(this); // added this
+    this.getMyLocation = this.getMyLocation.bind(this); // added this
+    this.getFriendsRambls = this.getFriendsRambls.bind(this);
+    this.handleRamblPress = this.handleRamblPress.bind(this);
   }
 
-  getMyRambls(){
+  getMyRambls() {
     return this.props.screenProps.past_rambls;
   }
 
-  getFriendsRambls(){
+  // added this
+  getMyUsername() {
+    return this.props.screenProps.globalState.username;
+  }
+  // added this
+  getMyLocation() {
+    return this.props.screenProps.globalState.location;
+  }
+
+  getFriendsRambls() {
     return this.props.screenProps.friends_rambls;
   }
 
-  _onValueChange(value){
-    this.setState({value: value});
+  _onValueChange(value) {
+    this.setState({ value: value });
     console.log(value);
   }
 
-  _onChange(event){
-    this.setState({selectedIndex: event.nativeEvent.selectedSegmentIndex});
+  _onChange(event) {
+    this.setState({ selectedIndex: event.nativeEvent.selectedSegmentIndex });
   }
 
-  handleRamblPress(rambl){
+  handleRamblPress(rambl) {
     this.setState({
       currentRambl: rambl,
-      currentView: "selected",
+      currentView: "selected"
     });
   }
 
-  render(){
+  render() {
     var displayView = null;
     if(this.state.currentView === "selected"){
       return (
@@ -81,8 +91,7 @@ export default class HistoryScreen extends React.Component {
           />
         </View>
       )
-    }
-    if(this.state.value === "Friends\' Rambls"){
+    }if(this.state.value == "Friends\' Rambls"){
       displayView = (
         <View style={{flex: 1, padding: 22}}>
           <FlatList
@@ -91,111 +100,110 @@ export default class HistoryScreen extends React.Component {
           />
         </View>
       )
-    }}
+    }
     return (
-      <View
-        style={{ flex: 1 }}
-      >
-      <View>
+      <View style={{ flex: 1 }}>
+        <View>
           <SegmentedControlIOS
             values={this.state.values}
             selectedIndex={this.state.selectedIndex}
-            onChange = {this._onChange}
-            onValueChange= {this._onValueChange} />
+            onChange={this._onChange}
+            onValueChange={this._onValueChange}
+          />
         </View>
         {displayView}
       </View>
     );
-
   }
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     top: 50,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   developmentModeText: {
     marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
+    color: "rgba(0,0,0,0.4)",
     fontSize: 14,
     lineHeight: 19,
-    textAlign: 'center',
+    textAlign: "center"
   },
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 30
   },
   welcomeContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
-    marginBottom: 20,
+    marginBottom: 20
   },
   welcomeImage: {
     width: 100,
     height: 80,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginTop: 3,
-    marginLeft: -10,
+    marginLeft: -10
   },
   getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+    alignItems: "center",
+    marginHorizontal: 50
   },
   homeScreenFilename: {
-    marginVertical: 7,
+    marginVertical: 7
   },
   codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+    color: "rgba(96,100,109, 0.8)"
   },
   codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
+    backgroundColor: "rgba(0,0,0,0.05)",
     borderRadius: 3,
-    paddingHorizontal: 4,
+    paddingHorizontal: 4
   },
   getStartedText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
+    color: "rgba(96,100,109, 1)",
     lineHeight: 24,
-    textAlign: 'center',
+    textAlign: "center"
   },
   tabBarInfoContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
     ...Platform.select({
       ios: {
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOffset: { height: -3 },
         shadowOpacity: 0.1,
-        shadowRadius: 3,
+        shadowRadius: 3
       },
       android: {
-        elevation: 20,
-      },
+        elevation: 20
+      }
     }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 20
   },
   tabBarInfoText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center"
   },
   navigationFilename: {
-    marginTop: 5,
+    marginTop: 5
   },
   helpContainer: {
     marginTop: 15,
-    alignItems: 'center',
+    alignItems: "center"
   },
   helpLink: {
-    paddingVertical: 15,
+    paddingVertical: 15
   },
   helpLinkText: {
     fontSize: 14,
-    color: '#2e78b7',
-  },
+    color: "#2e78b7"
+  }
 });
