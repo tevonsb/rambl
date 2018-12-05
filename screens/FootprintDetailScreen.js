25b61e91d8e5dfeab4fe2d81d55bff70f9cb13bb
Kally Zheng <kallyzheng@DNa1d66ca.SUNet>
2018-12-05 13:10:04 -0800

    ladida

--- screens/FootprintDetailScreen.js
+++ screens/FootprintDetailScreen.js
@@ -18,26 +18,21 @@ export default class FootprintDetailComponent extends React.Component {
     super(props);
   }
 
-  getAction(footprint){
+  getAction(){
     if(this.props.action === "Visit"){
-      if(footprint.visited !== true){
-        return (
-          <View style = {{
-              flex: 1,
-              alignItems: 'center',
-              alignSelf:'center',
-              justifyContent: 'center'
-            }}>
-            <View style = {{marginTop:5}}>
-            <TouchableOpacity onPress={()=> this.props.handleVisitPress(footprint)}>
-              <View style={this.props.screenProps.globalStyle.purpleButton}>
-                <Text style={this.props.screenProps.globalStyle.buttonText}>I just visited!</Text>
-              </View>
-            </TouchableOpacity>
-            </View>
-          </View>
-        )
-      }
+      return (
+        <View style = {{
+          // flex: 1,
+          alignItems: 'center',
+          marginTop: 10,
+        }}>
+        <TouchableOpacity onPress={()=> this.props.handleVisitPress}>
+        <View style={this.props.screenProps.globalStyle.purpleButton}>
+        <Text style={this.props.screenProps.globalStyle.buttonText}>Visit</Text>
+        </View>
+        </TouchableOpacity>
+        </View>
+      )
     }
     if(this.props.action === "Visited"){
       return (
@@ -49,23 +44,28 @@ export default class FootprintDetailComponent extends React.Component {
 
   getFootprints(){
     return this.props.rambl.footprints;
-  }
+    }
 
   render() {
-    return(
-      //<View style={{width: Dimensions.get('window').width-20, height: 250, backgroundColor: '#353535', padding: 10, marginTop: 5}}>
-      <View style={{flex:2, backgroundColor: '#353535', padding: 10, marginTop: 15}}>
-        <FlatList style={this.props.screenProps.globalStyle.flatlist}
-          data={this.getFootprints()}
-          renderItem={({item}) =>
-          <TouchableOpacity disabled={true} style={this.props.screenProps.globalStyle.footprintContainer}>
-            <Text style={this.props.screenProps.globalStyle.message}>{item.title}</Text>
-            <Text style={this.props.screenProps.globalStyle.footprintDetail}>Category: {item.category}</Text>
-            <Text style={this.props.screenProps.globalStyle.footprintDetail}>Description: {item.description}</Text>
-            <Text style={this.props.screenProps.globalStyle.footprintDetail}>Rating: {item.rating}/5.0</Text>
-            {this.getAction(item)}
-          </TouchableOpacity>} keyExtractor = {(item, index)=>index.toString()}/>
-        </View>
-      )
+        return(
+          <View style={{width: Dimensions.get('window').width-20, height: 250, backgroundColor: '#353535',padding:10, marginTop: 5}}>
+            <FlatList style={this.props.screenProps.globalStyle.flatlist}
+              data={this.getFootprints()}
+              renderItem={({item}) => <TouchableOpacity disabled={true} style={this.props.screenProps.globalStyle.rambl}>
+              <Text style={this.props.screenProps.globalStyle.message}>{item.title}</Text>
+              <Text style={this.props.screenProps.globalStyle.footprintDetail}>Category: {item.category}</Text>
+              <Text style={this.props.screenProps.globalStyle.footprintDetail}>Description: {item.description}</Text>
+             <Text style={this.props.screenProps.globalStyle.footprintDetail}>Rating: {item.rating}/5.0</Text>
+              </TouchableOpacity>} keyExtractor = {(item, index)=>index.toString()}/>
+              {this.getAction()}
+              </View>
+        )
+      }
     }
-  }
+
+    // <View style={this.props.screenProps.globalStyle.rambl}>
+    // <Text style={this.props.screenProps.globalStyle.footprintItem}>{this.props.footprint.title}</Text>
+    // <Text style={this.props.screenProps.globalStyle.detail}>Category: {this.props.footprint.category}</Text>
+    // <Text style={this.props.screenProps.globalStyle.detail}>Description: {this.props.footprint.description}</Text>
+    // {this.getAction()}
+    // </View>
